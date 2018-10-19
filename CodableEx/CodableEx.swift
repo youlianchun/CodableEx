0ebd840015cf4788022d47f7966bab9323f46f00
YLCHUN <>
2018-10-19 17:16:48 +0800

    review

--- CodableEx/CodableEx.swift
+++ CodableEx/CodableEx.swift
@@ -10,15 +10,10 @@ import Foundation
 
 //MARK: -
 //MARK: - CodableExProtocol
-
-public protocol CodableExProtocol : Codable
+public protocol CodableExProtocol:Codable
 {
     func finishEncode() -> Void
     func finishDecode() -> Void
-    
-    func encode() -> [String:Any]?
-    func encode() -> String?
-    func encode() -> Data?
 }
 
 extension CodableExProtocol
@@ -26,63 +21,48 @@ extension CodableExProtocol
     func finishEncode() -> Void {}
     func finishDecode() -> Void {}
     
-    func encode() -> [String:Any]?
-    {
-        return CodableEx().encode(self);
-    }
-    
-    func encode() -> String?
-    {
-        return CodableEx().encode(self);
-    }
-    
-    func encode() -> Data?
-    {
-        return CodableEx().encode(self);
+    func encode() throws -> [String:Any] {
+        return try CodableEx().encode(self)
     }
 }
 
 //MARK: -
 //MARK: - Dictionary & Array extension
-extension Dictionary where Key == String
-{
-    func decode<T:Decodable>() -> T?
-    {
-        return CodableEx().decode(self)
-    }
-}
-
-extension Array where Element == [String : Any]
-{
-    func decode<T:Decodable>() -> Array<T>?
-    {
-        return CodableEx().decode(self)
-    }
-}
-
 extension Array where Element:Encodable
 {
-    func encode() -> Array<[String:Any]>?
-    {
-        return CodableEx().encode(self)
+    func encode() throws -> Array<[String:Any]> {
+        return try CodableEx().encode(self)
     }
 }
 
-    //MARK: -
-    //MARK: - CodableEx
-private class CodableEx
+extension Dictionary where Key == String
 {
-    //MARK: - traverse
+    func decode<T:Decodable>() throws -> T {
+        return try CodableEx().decode(self)
+    }
+}
+
+extension Array where Element == [String:Any]
+{
+    func decode<T:Decodable>() throws -> Array<T> {
+        return try CodableEx().decode(self)
+    }
+}
+
+//MARK: -
+//MARK: - CodableEx
+final class CodableEx
+{
     //MARK: private
-    private func traverse(_ any : Any?, _ finish : (CodableExProtocol)->Void) -> Void
+    private func traverse(_ any:Any?, _ finish:(CodableExProtocol)->Void) -> Void
     {
         if let model = any as? CodableExProtocol
         {
-            finish(model)
             let mirror = Mirror(reflecting: model)
             for (_, value) in mirror.children {
                 traverse(value, finish)
             }
+            finish(model)
             return
         }
         if let models = any as? Array<CodableExProtocol>
@@ -93,83 +73,86 @@ private class CodableEx
         }
     }
     
-    //MARK: - decode
-    //MARK: private
-    private func decode<T>(json : String) -> T?
+
+    fileprivate func jsonStr2JsonObj<T>(_ jsonStr:String) throws -> T
     {
-        guard let data = json.data(using: .utf8) else { return nil }
-        let any = try? JSONSerialization.jsonObject(with: data, options: [.mutableContainers])
-        return any as? T
+        guard let data = jsonStr.data(using: .utf8)
+            else { throw NSError(domain: "Convert \"\(jsonStr)\" to Data data failed.", code: 1, userInfo: nil) }
+        let jsonObj = try JSONSerialization.jsonObject(with: data, options: [.mutableContainers])
+        guard let obj = jsonObj as? T
+            else { throw NSError(domain: "Convert \"\(jsonStr)\" to \(T.self) data failed.", code: 2, userInfo: nil) }
+        return obj
     }
     
-    private func decode(any : Any) -> Data?
-    {
-        let data = try? JSONSerialization.data(withJSONObject: any, options: .prettyPrinted)
-        return data
+    fileprivate func jsonObj2Data<T>(_ jsonObj:T) throws -> Data {
+        return try JSONSerialization.data(withJSONObject: jsonObj, options: .prettyPrinted)
     }
     
-    private func _decode<T, RT:Decodable>(_ any : T) -> RT?
+    fileprivate func jsonObj2Model<T, RT:Decodable>(_ any:T) throws -> RT
     {
-        guard let data:Data = decode(any:any) else { return nil }
-        let any = try? JSONDecoder().decode(RT.self, from: data)
+        let data = try jsonObj2Data(any)
+        let any = try JSONDecoder().decode(RT.self, from: data)
         traverse(any) { $0.finishDecode() }
         return any
     }
     
-    //MARK: public
-    public func decode<T:Decodable>(_ dict : [String : Any]) -> T?
-    {
-        return _decode(dict)
+    fileprivate func model2Data<T:Encodable>(_ any:T) throws -> Data {
+        return try JSONEncoder().encode(any)
     }
     
-    public func decode<T:Decodable>(_ arr : Array<[String : Any]>) -> Array<T>?
+    fileprivate func model2JsonObj<T:Encodable, RT>(_ any:T) throws -> RT
     {
-        return _decode(arr)
-    }
-    
-    public func decode<T:Decodable>(_ json : String) -> T?
-    {
-        guard let dict : [String:Any] = decode(json:json) else { return nil }
-        return decode(dict)
-    }
-    
-    public func decode<T:Decodable>(_ json : String) -> Array<T>?
-    {
-        guard let arr : [String:Any] = decode(json:json) else { return nil }
-        return decode(arr)
-    }
-    
-    //MARK: - encode
-    //MARK: private
-    private func _encode<T:Encodable, RT>(_ any : T) -> RT?
-    {
-        guard let data : Data = encode(any) else { return nil }
-        defer { traverse(any) { $0.finishEncode() } }
-        let res = try? JSONSerialization.jsonObject(with: data, options: [.mutableContainers])
-        return res as? RT
+        let data = try model2Data(any)
+        let res = try JSONSerialization.jsonObject(with: data, options: [.mutableContainers])
+        guard let rt = res as? RT
+            else { throw NSError(domain: "Convert \"\(any)\" to jsonObject failed.", code: 3, userInfo: nil) }
+        traverse(any) { $0.finishEncode() }
+        return rt
     }
     
     //MARK: public
-    public func encode<T:Encodable>(_ model : T) -> Data?
-    {
-        let data = try? JSONEncoder().encode(model)
-        return data
+    func decode<T:Decodable>(_ dict:[String:Any]) throws -> T {
+        return try jsonObj2Model(dict)
     }
     
-    public func encode<T:Encodable>(_ model:T) -> String?
-    {
-        guard let data : Data = encode(model) else { return nil }
-        return String(data: data, encoding: .utf8)
+    func decode<T:Decodable>(_ arr:Array<[String:Any]>) throws -> Array<T> {
+        return try jsonObj2Model(arr)
+    }
+
+    func encode<T:Encodable>(_ model:T) throws -> [String:Any] {
+        return try model2JsonObj(model)
     }
     
-    public func encode<T:Encodable>(_ model : T) -> [String:Any]?
-    {
-        return _encode(model)
+    func encode<T:Encodable>(_ models:Array<T>) throws -> Array<[String:Any]> {
+        return try model2JsonObj(models)
+    }
+}
+
+extension CodableEx
+{
+    func decode<T:Decodable>(_ json:String) throws -> T {
+        let dict:[String:Any] = try jsonStr2JsonObj(json)
+        return try decode(dict)
     }
     
-    public func encode<T:Encodable>(_ models : Array<T>) -> Array<[String : Any]>?
-    {
-        return _encode(models)
+    func decode<T:Decodable>(_ json:String) throws -> Array<T> {
+        let arr:[[String:Any]] = try jsonStr2JsonObj(json)
+        return try decode(arr)
     }
     
+    func encode<T:Encodable>(_ model:T) throws -> String
+    {
+        let data = try model2Data(model)
+        guard let ret = String(data: data, encoding: .utf8)
+            else { throw NSError(domain: "Convert \"\(model)\" to String data failed.", code: 4, userInfo: nil) }
+        return ret
+    }
+    
+    func encode<T:Encodable>(_ models:Array<T>) throws -> String
+    {
+        let data = try model2Data(models)
+        guard let ret = String(data: data, encoding: .utf8)
+            else { throw NSError(domain: "Convert \"\(models)\" to String data failed.", code: 4, userInfo: nil) }
+        return ret
+    }
 }
